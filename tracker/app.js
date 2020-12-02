983299aa3f2d70cf99f33f9fa439b0745b00350f
Wulfy <wulf38@gmail.com>
2020-12-02 06:06:31 -0500

    Disable auto-tracking by default

--- tracker/app.js
+++ tracker/app.js
@@ -24,7 +24,7 @@ function blank_tracker() {
         },
         notes: "",
         autotrack: {
-            enabled: true,
+            enabled: false,
             status: "",
         },
     };
